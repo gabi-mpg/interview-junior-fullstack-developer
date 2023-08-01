73720d285c78a0a3e0f00d124b747293f52be7df
Gabriela <gabrielamercadoperez@gmail.com>
2023-08-01 11:49:09 +0100

    First commit, components created

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { CityListComponent } from './city-list/city-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitySearchComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
