f822829ea48479e670d174a88b38101c9acd6405
Gabriela <gabrielamercadoperez@gmail.com>
2023-08-01 14:23:52 +0100

    First version front end

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -4,8 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CitySearchComponent } from './city-search/city-search.component';
-import { CityListComponent } from './city-list/city-list.component';
+import { CitySearchComponent } from './components/city-search/city-search.component';
+import { CityListComponent } from './components/city-list/city-list.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +26,14 @@ import { CityListComponent } from './city-list/city-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
